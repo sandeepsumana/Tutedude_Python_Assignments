6452bb65ea174565b026447ca75677e4cb3b5810
sandeepsumana <ramamurthysandeep@gmail.com>
2025-08-17 18:20:55 +0530

    Add files via upload

---
+++ Assignment1/Assigment1_Sandeep_Task2.py
@@ -0,0 +1,3 @@
+a = input("Enter your first name")
+b = input("Enter your last name")
+print("Welcome to the world of python programming",a,b)
---
+++ Assignment1/Assignment1_Sandeep_Task1.py
@@ -0,0 +1,7 @@
+a = int(input("Enter an integer"))
+b= int(input("Enter another integer"))
+print("Addition:",str(a+b))
+print("Subtraction:",str(a-b))
+print("Multiplication:",str(a*b))
+print("Division:",str(a/b))
+
